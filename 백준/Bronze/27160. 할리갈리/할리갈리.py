80e993ff7b7cbc1449cafa58576b14c7eb676f5e
kimchoker <121940381+kimchoker@users.noreply.github.com>
2024-07-20 14:05:20 +0900

    [Bronze II] Title: 할리갈리, Time: 4240 ms, Memory: 46236 KB -BaekjoonHub

---
+++ 백준/Bronze/27160. 할리갈리/할리갈리.py
@@ -0,0 +1,29 @@
+count = int(input())
+input_data = []
+
+
+cards = [['STRAWBERRY', 0], ['BANANA', 0], ['LIME', 0], ['PLUM', 0]]
+
+
+for i in range(count):
+    input_data.append(list(input().split(' ')))
+
+
+#
+def update_card_value(samples, data):
+    for fruit, value in data:
+        value = int(value)
+        for sample in samples:
+            if sample[0] == fruit:
+                sample[1] += value
+
+
+update_card_value(cards, input_data)
+
+
+isContain = any(card[1] == 5 for card in cards)
+
+if isContain:
+    print('YES')
+else:
+    print('NO')
