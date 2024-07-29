0c6784edaaa86fd2b29ad9ef485ca79bea2eed19
kimchoker <121940381+kimchoker@users.noreply.github.com>
2024-07-29 20:12:51 +0900

    [Bronze I] Title: 콘서트, Time: 420 ms, Memory: 185176 KB -BaekjoonHub

--- 백준/Bronze/16466. 콘서트/콘서트.py
+++ 백준/Bronze/16466. 콘서트/콘서트.py
@@ -1,12 +1,12 @@
 import sys
 
-# 1차 티켓팅에서 팔린 티켓들의 수를 입력
+# 1차 티켓팅에서 팔린 티켓들의 수를 입력받습니다.
 num_count = int(input().strip())
 
-# 1차 티켓팅에서 팔린 티켓들의 번호를 입력받아 집합으로 만들기
+# 1차 티켓팅에서 팔린 티켓들의 번호를 입력받아 집합으로 만듭니다.
 sold = set(map(int, sys.stdin.readline().strip().split()))
 
-# 1번부터 차례대로 확인하여 가장 작은 사용 가능한 티켓 번호를 찾기
+# 1번부터 차례대로 확인하여 가장 작은 사용 가능한 티켓 번호를 찾습니다.
 ticket_number = 1
 while ticket_number in sold:
     ticket_number += 1
