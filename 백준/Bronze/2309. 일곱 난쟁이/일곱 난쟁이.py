62fb0542b68a67ecb29e0a730ec7903ef8097615
kimchoker <121940381+kimchoker@users.noreply.github.com>
2024-07-29 14:32:00 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,27 @@
+import sys
+
+# 아홉 난쟁이의 키를 입력받습니다.
+height = [int(sys.stdin.readline().strip()) for _ in range(9)]
+
+# 전체 합에서 100을 뺀 값이 제거해야 할 두 난쟁이의 키의 합입니다.
+timid = sum(height) - 100
+
+# 두 난쟁이의 키를 찾습니다.
+found = False
+for i in range(9):
+    for j in range(i + 1, 9):
+        if height[i] + height[j] == timid:
+            suspect1, suspect2 = height[i], height[j]
+            found = True
+            break
+    if found:
+        break
+
+# 찾은 두 난쟁이의 키를 리스트에서 제거합니다.
+height.remove(suspect1)
+height.remove(suspect2)
+
+# 정렬하여 출력합니다.
+height.sort()
+for dwarf in height:
+    print(dwarf)
