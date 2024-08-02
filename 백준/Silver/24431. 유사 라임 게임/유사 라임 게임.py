98a2a28a09404fd4cc873781fe098ca9f619c254
kimchoker <121940381+kimchoker@users.noreply.github.com>
2024-08-02 15:16:17 +0900

    [Silver IV] Title: 유사 라임 게임, Time: 268 ms, Memory: 34000 KB -BaekjoonHub

--- 백준/Silver/24431. 유사 라임 게임/유사 라임 게임.py
+++ 백준/Silver/24431. 유사 라임 게임/유사 라임 게임.py
@@ -1,21 +1,25 @@
 import sys
+from collections import deque
 
 test_case = int(sys.stdin.readline().strip())
 
 for i in range(test_case):
     word_count, word_length, rhyme_length = map(int, sys.stdin.readline().strip().split())
-    words = sys.stdin.readline().strip().split()
+    words = deque(sys.stdin.readline().strip().split())
     count = 0
 
-    for _ in range(len(words)):
-        if not words:
-            break
-        first_word = words.pop(0)
-        # print(first_word)
-        filtered = [item for item in words if item and item.endswith(first_word[-rhyme_length:])]
+    while words:
+        # 덱의 첫 번째 요소를 pop
+        first_word = words.popleft()
+        # pop 해온 요소의 접미사와 같은 요소(.endswith())를 찾아 filtered에 담음
+        filtered = [word for word in words if word and word.endswith(first_word[-rhyme_length:])]
+        # filtered가 비어있지 않으면 = 쌍이 될 수 있는 요소가 있으면
         if filtered:
+            # 카운트를 한 개 올려줌
             count += 1
-            popped = filtered.pop()
-            words.remove(popped)
+            # 쌍이 될 수 있는 요소가 아무리 많다고 해도 한 번밖에 사용할 수 없으므로 pop
+            used = filtered.pop()
+            # filtered에서 사용된 요소를 words에서 제거
+            words.remove(used)
 
     print(count)
